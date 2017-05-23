d7189b311f530924a4c2166ecacc26054612b981
Courtin Simon <courtin.simon@gmail.com>
2017-05-23 14:17:22 +0200

    Site de demonstration

--- Demo/Includes/footer.inc.php
+++ Demo/Includes/footer.inc.php
@@ -1,4 +1,10 @@
 <footer>
-    <p>© 2016 Copyright</p>
-	<p>Fièrement propulsé par <a href="https://github.com/simoncourtin/Parapluie">Parapluie</a></p>
+  <p>Formulaire de <a href="index.php?page=-Contact">Contact</a></p>
+  <p>Textes et images venant exclusivement de Wikipédia
+    (<a href="https://fr.wikipedia.org/wiki/Parapluie">https://fr.wikipedia.org/wiki/Parapluie</a>,
+    <a href="https://fr.wikipedia.org/wiki/Ombrelle">https://fr.wikipedia.org/wiki/Ombrelle</a>).
+  </p>
+  <div id="footer-copyright" >
+    <p>© 2016 - <?php echo date('Y'); ?> Copyright. Fièrement propulsé par <a href="https://github.com/simoncourtin/Parapluie">Parapluie</a></p>
+  <div>
 </footer>
