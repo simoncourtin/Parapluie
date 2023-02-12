47c9075b09d40d78afd8f074b170603989111f25
Simon Courtin <simon.courtin@outlook.fr>
2023-02-12 15:53:19 +0100

    Parapluie v.1 - prod

--- Includes/footer.inc.php
+++ Includes/footer.inc.php
@@ -1,6 +1,6 @@
 <footer class="page-footer light-blue lighten-1">
 
   <div class="footer-copyright">
-      © 2016-<?php echo date("Y");?> Copyright. Fièrement propulsé par <a href="https://github.com/simoncourtin/Parapluie">Parapluie</a>
+      © 2016-<?php echo date("Y");?> Copyright. Fièrement propulsé par <a href="https://github.com/simoncourtin/Parapluie">Parapluie (v.1.0)</a>
   </div>
 </footer>
